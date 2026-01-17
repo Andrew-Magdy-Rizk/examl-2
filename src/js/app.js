7006a220d70367e56a44092683d2245f8ee13399
Andrew <andrewdakran72@gmail.com>
2026-01-17 20:02:50 +0200

    update

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,8 @@ export default class App {
     #navLinks = document.querySelectorAll("#links li a");
     #router = new Router();
     initApp() {
-        this.#router.navigation("/home");
+        const path = location.pathname;
+        path === "/" ? this.#router.navigation("/home") : this.#router.navigation(path);
 
         this.#navLinks.forEach((link) => {
             link.addEventListener("click", (e) => {
