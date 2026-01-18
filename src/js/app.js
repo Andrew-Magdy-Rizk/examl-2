983e160329e42315f05fae28e2fadee6686b4648
Andrew <andrewdakran72@gmail.com>
2026-01-18 15:37:00 +0200

    update

--- src/js/app.js
+++ src/js/app.js
@@ -3,18 +3,37 @@ import Router from "./route.js";
 export default class App {
     #navLinks = document.querySelectorAll("#links li a");
     #router = new Router();
+    #sidebarOpen = document.getElementById("header-menu-btn");
+    #sidebarOverlay = document.getElementById("sidebar-overlay");
+    #sidebar = document.getElementById("sidebar");
+    #sidebarClose = document.getElementById("sidebar-close-btn");
+
     initApp() {
         const path = location.pathname;
-        path === "/" ? this.#router.navigation("/home") : this.#router.navigation(path);
+        path === "/" || path === "/index.html" ? this.#router.navigation("/home") : this.#router.navigation(path);
 
         this.#navLinks.forEach((link) => {
             link.addEventListener("click", (e) => {
                 e.preventDefault();
                 const path = link.getAttribute("href");
                 this.#router.navigation(path);
+                this.sidebartoggle();
             });
-
         });
 
+        this.#sidebarOpen.addEventListener("click", () => {
+            this.sidebartoggle();
+
+        })
+        this.#sidebarClose.addEventListener("click", () => {
+            this.sidebartoggle();
+        })
+
+
+    }
+
+    sidebartoggle() {
+        this.#sidebarOverlay.classList.toggle("active");
+        this.#sidebar.classList.toggle("open");
     }
 }
--- src/js/route.js
+++ src/js/route.js
@@ -4,6 +4,12 @@ import Home from "./modules/home.module.js";
 export default class Router {
     #mealsApi = new MealsApi();
     #loadingPage = document.getElementById("app-loading-overlay");
+    #homePage = document.getElementById("home-page");
+    #mealDetailsPage = document.getElementById("meal-details");
+    #productPage = document.getElementById("products-section");
+    #foodlogPage = document.getElementById("foodlog-section");
+    #headTitle = document.getElementById("head-title").children[0];
+    #headDescription = document.getElementById("head-title").children[1];
     navigation(route) {
 
         switch (route) {
@@ -19,7 +25,7 @@ export default class Router {
                 // history.pushState({}, "", route);
                 this.#toFoodLog()
                 break;
-                case /^\/products\/\w+/.test(route):
+            case /^\/products\/\w+/.test(route):
                 // history.pushState({}, "", route);
                 this.#toMealDetails()
                 break;
@@ -30,12 +36,19 @@ export default class Router {
     }
 
     async #toHome() {
+        console.log("home");
         this.#toLoading();
         const categories = await this.#mealsApi.getCategories();
         const areas = await this.#mealsApi.getAreas();
         const meals = await this.#mealsApi.getMeals("chicken");
         this.#toLoading();
-        console.log("home");
+        this.#homePage.classList.remove("hidden");
+        this.#mealDetailsPage.classList.add("hidden");
+        this.#productPage.classList.add("hidden");
+        this.#foodlogPage.classList.add("hidden");
+        
+        this.#headTitle.innerHTML = 'Meals & Recipes';
+        this.#headDescription.innerHTML = 'Discover delicious and nutritious recipes tailored for you';
         new Home({ categories, areas, meals });
 
 
@@ -43,15 +56,33 @@ export default class Router {
 
     #toMealDetails() {
         console.log("meal Detials");
-
+        this.#mealDetailsPage.classList.remove("hidden");
+        this.#homePage.classList.add("hidden");
+        this.#productPage.classList.add("hidden");
+        this.#foodlogPage.classList.add("hidden");
+        this.#headTitle.innerHTML = 'Recipe Details';
+        this.#headDescription.innerHTML = 'View full recipe information and nutrition facts';
+        
+        
     }
     #toProduct() {
         console.log("Products");
-
+        this.#productPage.classList.remove("hidden");
+        this.#homePage.classList.add("hidden");
+        this.#mealDetailsPage.classList.add("hidden");
+        this.#foodlogPage.classList.add("hidden");
+        this.#headTitle.innerHTML = 'Product Scanner';
+        this.#headDescription.innerHTML = 'Search packaged foods by name or barcode';
+        
     }
     #toFoodLog() {
         console.log("FoodLog");
-
+        this.#foodlogPage.classList.remove("hidden");
+        this.#homePage.classList.add("hidden");
+        this.#mealDetailsPage.classList.add("hidden");
+        this.#productPage.classList.add("hidden");
+        this.#headTitle.innerHTML = 'Food Log';
+        this.#headDescription.innerHTML = 'Track your daily nutrition and food intake';
     }
 
     #toLoading() {
